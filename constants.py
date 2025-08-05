857bb10d4eded5716130468760bda7bfa5c56f3b
PrithviMokashi <mokashiprithviraj@gmail.com>
2025-08-06 00:03:11 +0530

    Write stream to delta & query delta tables

--- constants.py
+++ constants.py
@@ -1,2 +1,7 @@
+import os
+
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 KAFKA_TOPIC_NAME = "nyc_taxi_topic"
+PARQUET_PATH = os.path.join(os.getcwd(),'datasets/yellow_tripdata_2025-01.parquet')
+BATCH_SIZE = 10000
+SLEEP_BETWEEN_BATCHES = 2
